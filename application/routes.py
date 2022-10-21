febd78415156199343b2cfb280772645277e1dfd
unknown <neotaxman007@gmail.com>
2022-10-21 02:02:09 +0100

    Prototype v1.3

--- application/routes.py
+++ application/routes.py
@@ -15,7 +15,7 @@ def index():
 #CREATE the duel monster type.
 #Location for the app function: ip_address:5000/monstertype
 @app.route("/monstertype", methods=["POST", "GET"])
-def typeadd():
+def addtype():
     form = TypeForm()
     if form.validate_on_submit():
         mtypes = MonsterType(
@@ -29,7 +29,7 @@ def typeadd():
 #CREATE the duel monster card name.
 #Location for the app function: ip_address:5000/monstername
 @app.route("/monstername", methods=["POST", "GET"])
-def add():
+def addname():
     #Navigates to MonsterForm
     form = Monsterform
     #Validates if the user entered the submit button
@@ -53,7 +53,7 @@ def add():
 #UPDATE the duel monster type
 #Location for the app function: ip_address:5000/updatemonstertype
 @app.route("/updatemonstertype/<int:id>", methods=["GET", "POST"])
-def updatemonstertype(id):
+def updatetype(id):
     form = TypeForm()
     mtypes = MonsterType.query.get(id)
     if form.validate_on_submit():
@@ -67,7 +67,7 @@ def updatemonstertype(id):
 #UPDATE the duel monster name.
 #Location for the app function: ip_address:5000/updatemonstername
 @app.route("/updatemonstername/<int:id>", methods=["GET", "POST"])
-def update(id):
+def updatename(id):
     form = Monsterform()
     #Retrieves one duel monster name via specified ID.
     name = MonsterName.query.get(id)
@@ -100,7 +100,7 @@ def deletetype(id):
 #DELETE duel monster name
 #Location for the app function: ip_address:5000/deletename
 @app.route("/deletename/<int:id>")
-def delete(id):
+def deletename(id):
     #Relative to the data the user will delete via the ID
     name = MonsterName.query.get(id)
     #Deleting the item from the database
@@ -108,17 +108,10 @@ def delete(id):
     #Commit the new changes
     db.session.commit()
     #Returnthe URL via the index function. 
-    return redirect(url_for('index'))        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    return redirect(url_for('index')) 
+
+
+
+
+
+
